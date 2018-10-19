7b1a824dda882da561fda4abd2c06c139782b584
danilovalerio <danilo_vs@hotmail.com>
2018-10-19 16:55:13 -0300

    Adicionado contratos para comparar objetos de forma prática

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -27,9 +27,16 @@ public class Funcionario implements Comparable<Funcionario> {
 	}
 
 	@Override
-	public int compareTo(Funcionario func) {//Comparação de objetos
+	public int compareTo(Funcionario func) {//Comparação e ordenação de objetos por nome
 		
-		return nome.compareTo(func.getNome());
+		//return nome.compareTo(func.getNome());
+		
+		//se quiser comparar por salário ordem CRESCENTE
+		//return salario.compareTo(func.getSalario());
+		
+		//se quiser comparar por salário ordem DECRESCENTE
+		return -salario.compareTo(func.getSalario());
+		
 	}
 	
 	
